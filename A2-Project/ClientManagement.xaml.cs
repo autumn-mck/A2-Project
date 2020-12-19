380f0313553e6b45d52e561cc8dffbb83e8df925
James McKee <jamesomkcee@icloud.com>
2020-12-19 21:19:11 +0000

    Started work on contact editing window

--- A2-Project/ClientManagement.xaml.cs
+++ A2-Project/ClientManagement.xaml.cs
@@ -18,18 +18,59 @@ namespace A2_Project
 		private List<string> tableHeaders;
 		private List<List<string>> originalData;
 
+		List<UIElement> display = new List<UIElement>();
+
 		public ClientManagement(DBAccess _dbAccess)
 		{
 			InitializeComponent();
 			dbAccess = _dbAccess;
 
 			List<List<string>> data = dbAccess.GetAllFromTable("Contact");
-			DtgMethods.CreateTable(data, "Contact", dbAccess, ref dtgTest, ref tableHeaders, ref table);
+			DtgMethods.CreateTable(data, "Contact", dbAccess, ref dtgContacts, ref tableHeaders, ref table);
 			List<string> colSearch = new List<string> { "All Columns" };
 			colSearch.AddRange(tableHeaders);
 			cmbColumn.SelectedIndex = 0;
 			cmbColumn.ItemsSource = colSearch;
 			originalData = data;
+
+
+			for (int i = 0; i < tableHeaders.Count; i++)
+			{
+				Label lbl = new Label()
+				{
+					Content = tableHeaders[i],
+					Margin = new Thickness(900, i * 75 + 40, 0, 0),
+					HorizontalAlignment = HorizontalAlignment.Left,
+					VerticalAlignment = VerticalAlignment.Top
+				};
+				grd.Children.Add(lbl);
+				if (dbAccess.IsColumnPrimaryKey(tableHeaders[i], "Contact"))
+				{
+					lbl = new Label()
+					{
+						Content = "test",
+						Margin = new Thickness(900, i * 75 + 75, 0, 0),
+						HorizontalAlignment = HorizontalAlignment.Left,
+						VerticalAlignment = VerticalAlignment.Top
+					};
+					display.Add(lbl);
+					grd.Children.Add(lbl);
+				}
+				else
+				{
+					TextBox tbx = new TextBox()
+					{
+						Width = 300,
+						Height = 34,
+						Margin = new Thickness(905, i * 75 + 75, 0, 0),
+						FontSize = 24,
+						HorizontalAlignment = HorizontalAlignment.Left,
+						VerticalAlignment = VerticalAlignment.Top
+					};
+					display.Add(tbx);
+					grd.Children.Add(tbx);
+				}
+			}
 		}
 
 		private void BtnEmail_Click(object sender, RoutedEventArgs e)
@@ -59,10 +100,10 @@ namespace A2_Project
 
 		private void Search()
 		{
-			DtgMethods.UpdateSearch(originalData, cmbColumn.SelectedIndex, tbxSearch.Text, "Contact", dbAccess, ref dtgTest, ref tableHeaders, ref table);
+			DtgMethods.UpdateSearch(originalData, cmbColumn.SelectedIndex, tbxSearch.Text, "Contact", dbAccess, ref dtgContacts, ref tableHeaders, ref table);
 		}
 
-		private void DtgTest_AutoGeneratingColumn(object sender, DataGridAutoGeneratingColumnEventArgs e)
+		private void Dtg_AutoGeneratingColumn(object sender, DataGridAutoGeneratingColumnEventArgs e)
 		{
 			// Cancel the auto generated column
 			e.Cancel = true;
@@ -82,6 +123,29 @@ namespace A2_Project
 
 			// Add column back to data grid
 			if (sender is DataGrid dg) dg.Columns.Add(dgtc);
+		}
+
+		private void DtgContacts_SelectedCellsChanged(object sender, SelectedCellsChangedEventArgs e)
+		{
+			try
+			{
+				DataRowView drv = (DataRowView)dtgContacts.SelectedItems[0];
+				string contactID = (string)drv.Row.ItemArray[1];
+				dtgContactsToClient.Columns.Clear(); // TODO: Why is this needed here, but not elsewhere????
+				DtgMethods.CreateTable(dbAccess.GetContactsByClientID(contactID), "Contact", dbAccess, ref dtgContactsToClient, ref tableHeaders, ref table);
+				dtgContactsToClient.SelectedIndex = 0;
+			}
+			catch { }
+		}
+
+		private void DtgContactsToClient_SelectedCellsChanged(object sender, SelectedCellsChangedEventArgs e)
+		{
+			string[] selected = ((DataRowView)dtgContactsToClient.SelectedItems[0]).Row.ItemArray.OfType<string>().ToArray(); ;
+			for (int i = 0; i < selected.Length; i++)
+			{
+				if (display[i] is Label l) l.Content = selected[i];
+				else if (display[i] is TextBox t) t.Text = selected[i];
+			}
 		}
 	}
 }
