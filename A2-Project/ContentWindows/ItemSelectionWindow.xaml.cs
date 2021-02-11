8c809188140ac32b9ac2a5f410a14d1d47e5d4e9
James McKee <jamesomkcee@icloud.com>
2021-02-11 11:19:57 +0000

    Updates to adding window Minor improvements to data editing sidebar

---
+++ A2-Project/ContentWindows/ItemSelectionWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace A2_Project.ContentWindows
+{
+	/// <summary>
+	/// Interaction logic for ItemSelectionWindow.xaml
+	/// </summary>
+	public partial class ItemSelectionWindow : Window
+	{
+		SearchableDataGrid dtg;
+		UserControls.ValidatedItem parent;
+
+		public ItemSelectionWindow(UserControls.ValidatedItem _parent)
+		{
+			InitializeComponent();
+			parent = _parent;
+			string refTable = parent.Column.Constraints.ForeignKey.ReferencedTable;
+			dtg = new SearchableDataGrid(600, 400, refTable,
+			DBMethods.MetaRequests.GetColumnDataFromTable(refTable), this);
+			lblDtg.Content = dtg.Content;
+			
+			Height = 660;
+			Width = 700;
+
+			Button btnConfirm = new Button()
+			{
+				Content = "Confirm Selection"
+			};
+			btnConfirm.Click += BtnConfirm_Click;
+			grd.Children.Add(btnConfirm);
+		}
+
+		private void BtnConfirm_Click(object sender, RoutedEventArgs e)
+		{
+			parent.Text = dtg.GetSelectedData()[0];
+			Close();
+		}
+	}
+}
