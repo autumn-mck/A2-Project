cc8540a9243affd23b2cd34c795d9bb9c8e88cc7
James McKee <jamesomkcee@icloud.com>
2021-03-31 14:41:22 +0100

    Upgraded password security

--- A2-Project/DBMethods/LogRegRequests.cs
+++ A2-Project/DBMethods/LogRegRequests.cs
@@ -25,7 +25,7 @@ namespace A2_Project.DBMethods
 			if (uses2FA) strUses2FA = "1";
 			else strUses2FA = "0";
 			string salt = EmailManagement.GenerateRandomKey(32);
-			string hash = ComputeHash(staffPassword + salt);
+			string hash = GetSecureHash(staffPassword, salt);
 			DBAccess.ExecuteNonQuery($"INSERT INTO [Staff] VALUES ((SELECT Max([Staff ID]) FROM [Staff]) + 1, '{staffName}', '{hash}', '{salt}', '{staffEmail}', '{staffPhoneNo}', {strUses2FA});");
 		}
 
@@ -33,7 +33,17 @@ namespace A2_Project.DBMethods
 		{
 			List<List<string>> userData = DBAccess.GetListStringsWithQuery($"SELECT * FROM [Staff] WHERE [Staff].[Staff Name] = '{name}';");
 			if (userData.Count == 0) return false;
-			else return ComputeHash(password + userData[0][3]) == userData[0][2];
+			else return GetSecureHash(password, userData[0][3]) == userData[0][2];
+		}
+
+		private static string GetSecureHash(string password, string salt)
+		{
+			// Password, salt and pepper
+			string hash = ComputeHash(password + salt + "gTcZB660KJZLZTPNI4VJWG0pX0OqVpNK");
+
+			for (int i = 0; i < 10; i++) hash = ComputeHash(hash);
+
+			return hash;
 		}
 
 		/// <summary>
@@ -42,7 +52,7 @@ namespace A2_Project.DBMethods
 		private static string ComputeHash(string toHash)
 		{
 			byte[] bytes = Array.Empty<byte>();
-			using (SHA256 sha256Hash = SHA256.Create())
+			using (SHA512 sha256Hash = SHA512.Create())
 			{
 				bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(toHash));
 			}
