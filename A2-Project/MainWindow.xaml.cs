f9d0ddc31d4359aa4854955c9fd7802a6bbf6974
Autumn McKee <autumn@mck.is>
2021-04-12 10:52:33 +0100

    When the main window is resized, its aspect ratio is maintained.

--- A2-Project/MainWindow.xaml.cs
+++ A2-Project/MainWindow.xaml.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Shapes;
 using A2_Project.ContentWindows;
@@ -17,6 +19,18 @@ namespace A2_Project
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		private double _aspectRatio;
+		private bool? _adjustingHeight = null;
+		internal enum SWP
+		{
+			NOMOVE = 0x0002
+		}
+		internal enum WM
+		{
+			WINDOWPOSCHANGING = 0x0046,
+			EXITSIZEMOVE = 0x0232,
+		}
+
 		// Represents the direction the sidebar should go in when next animating. Oscillates between 1 and -1.
 		private int menuDirection = 1;
 		// Used to ensure any extra threads properly stop when the application is closing
@@ -79,6 +93,90 @@ namespace A2_Project
 			// Tries to get the user to log in
 			loginWindow = new Login();
 			lblContents.Content = loginWindow.Content;
+
+			this.SourceInitialized += Window_SourceInitialized;
+		}
+
+		[StructLayout(LayoutKind.Sequential)]
+		internal struct WINDOWPOS
+		{
+			public IntPtr hwnd;
+			public IntPtr hwndInsertAfter;
+			public int x;
+			public int y;
+			public int cx;
+			public int cy;
+			public int flags;
+		}
+
+		[DllImport("user32.dll")]
+		[return: MarshalAs(UnmanagedType.Bool)]
+		internal static extern bool GetCursorPos(ref Win32Point pt);
+
+		[StructLayout(LayoutKind.Sequential)]
+		internal struct Win32Point
+		{
+			public Int32 X;
+			public Int32 Y;
+		};
+
+		public static Point GetMousePosition() // mouse position relative to screen
+		{
+			Win32Point w32Mouse = new Win32Point();
+			GetCursorPos(ref w32Mouse);
+			return new Point(w32Mouse.X, w32Mouse.Y);
+		}
+
+		private void Window_SourceInitialized(object sender, EventArgs ea)
+		{
+			HwndSource hwndSource = (HwndSource)HwndSource.FromVisual((Window)sender);
+			hwndSource.AddHook(DragHook);
+
+			_aspectRatio = this.Width / this.Height;
+		}
+
+		private IntPtr DragHook(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+		{
+			switch ((WM)msg)
+			{
+				case WM.WINDOWPOSCHANGING:
+					{
+						WINDOWPOS pos = (WINDOWPOS)Marshal.PtrToStructure(lParam, typeof(WINDOWPOS));
+
+						if ((pos.flags & (int)SWP.NOMOVE) != 0)
+							return IntPtr.Zero;
+
+						Window wnd = (Window)HwndSource.FromHwnd(hwnd).RootVisual;
+						if (wnd == null)
+							return IntPtr.Zero;
+
+						// determine what dimension is changed by detecting the mouse position relative to the 
+						// window bounds. if gripped in the corner, either will work.
+						if (!_adjustingHeight.HasValue)
+						{
+							Point p = GetMousePosition();
+
+							double diffWidth = Math.Min(Math.Abs(p.X - pos.x), Math.Abs(p.X - pos.x - pos.cx));
+							double diffHeight = Math.Min(Math.Abs(p.Y - pos.y), Math.Abs(p.Y - pos.y - pos.cy));
+
+							_adjustingHeight = diffHeight > diffWidth;
+						}
+
+						if (_adjustingHeight.Value)
+							pos.cy = (int)(pos.cx / _aspectRatio); // adjusting height to width change
+						else
+							pos.cx = (int)(pos.cy * _aspectRatio); // adjusting width to heigth change
+
+						Marshal.StructureToPtr(pos, lParam, true);
+						handled = true;
+					}
+					break;
+				case WM.EXITSIZEMOVE:
+					_adjustingHeight = null; // reset adjustment dimension and detect again next time window is resized
+					break;
+			}
+
+			return IntPtr.Zero;
 		}
 
 		private void InitialiseDBConnection()
